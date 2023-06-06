57e8c5552f5caf55fadb0186b436b3e8e1a53b82
Matt Solomon <matt@mattsolomon.dev>
2023-06-05 17:18:46 -0700

    feat: add warning banner

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,10 +2,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 import logo from 'public/logo.svg';
 import { COMPANY_NAME } from '@/lib/constants';
+import { ExternalLink } from './ExternalLink';
 
 export const Header = () => {
   return (
     <header>
+      <div className='bg-yellow-500 py-2 text-center text-sm font-semibold dark:bg-yellow-600'>
+        This site is a work in progress and should not yet be relied on! Check out{' '}
+        <ExternalLink
+          className='text-black underline dark:text-white'
+          href='https://github.com/mds1/evm-diff'
+          text='the repo'
+        />{' '}
+        if you want to contribute.
+      </div>
       <div>
         <div className='flex items-center justify-between px-4 py-6 sm:px-6 md:space-x-10'>
           <div>
