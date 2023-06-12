4a2104203f111486d1028e02f2b55d08dc760aa9
Matt Solomon <matt@mattsolomon.dev>
2023-06-12 14:59:04 -0700

    chore: add mainnet sig types

--- src/chains/mainnet/index.ts
+++ src/chains/mainnet/index.ts
@@ -1,8 +1,10 @@
 import { mainnet as mainnetMetadata } from '@wagmi/chains';
 import { Chain } from '@/chains';
+import { signatureTypes } from './signatureTypes';
 import { precompiles } from './vm/precompiles';
 
 export const mainnet: Chain = {
   metadata: mainnetMetadata,
   precompiles,
+  signatureTypes,
 };
