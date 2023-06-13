c330cd9286000c62e2fcb7aba716451d04ee394a
leovct <l009.vincent@gmail.com>
2023-06-13 17:37:45 +0200

    feat: add back diff between Ethereum and Arbitrum `block.number` opcode

--- src/chains/arbitrum/vm/opcodes/index.ts
+++ src/chains/arbitrum/vm/opcodes/index.ts
@@ -1,8 +1,10 @@
 import { opcodes as mainnetOpcodes } from '@/chains/mainnet/vm/opcodes';
 import { OpcodeMap } from '@/chains/types';
 import { coinbase } from './block/coinbase';
+import { number } from './block/number';
 
 export const opcodes: OpcodeMap = {
   ...mainnetOpcodes,
   0x41: coinbase,
+  0x43: number,
 };
