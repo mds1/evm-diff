f26d24a61247b49bba372f28037a1f6ab344c57c
leovct <l009.vincent@gmail.com>
2023-06-13 18:01:02 +0200

    doc: update arbitrum `number` opcode

--- src/chains/arbitrum/vm/opcodes/block/number.ts
+++ src/chains/arbitrum/vm/opcodes/block/number.ts
@@ -3,12 +3,12 @@ import { Opcode } from '@/chains/types';
 export const number: Opcode = {
   number: 0x43,
   name: 'number',
-  description: "Get the L1 (Ethereum) block's number",
+  description: "Get the L1 block number",
   minGas: 2,
   outputs: [
     {
       name: 'blockNumber',
-      description: 'The current block number',
+      description: 'The L1 block number',
     },
   ],
   examples: [
