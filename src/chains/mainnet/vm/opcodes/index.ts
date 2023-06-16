d7b2e7a0d9e20bb9831509abe3c561ce27702723
leovct <l009.vincent@gmail.com>
2023-06-16 09:37:53 +0200

    fix: issue with imports

--- src/chains/mainnet/vm/opcodes/index.ts
+++ src/chains/mainnet/vm/opcodes/index.ts
@@ -1,7 +1,7 @@
 import { Opcode } from '@/types';
 import { opcodes as arithmeticOpcodes } from './arithmetic';
-import { opcodes as blockOpcodes } from './arithmetic';
-import { opcodes as memoryOpcodes } from './arithmetic';
+import { opcodes as blockOpcodes } from './block';
+import { opcodes as memoryOpcodes } from './memory';
 
 export const opcodes: Record<number, Opcode> = {
   ...arithmeticOpcodes,
