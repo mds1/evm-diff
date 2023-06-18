0a1c606d09564877fe37b95f5b21e063438fde82
leovct <l009.vincent@gmail.com>
2023-06-18 19:21:17 +0200

    refactor: update log opcodes

--- src/chains/mainnet/vm/opcodes/log/index.ts
+++ src/chains/mainnet/vm/opcodes/log/index.ts
@@ -1,14 +1,6 @@
 import { Opcode } from '@/types';
-import { log0 } from './log0';
-import { log1 } from './log1';
-import { log2 } from './log2';
-import { log3 } from './log3';
-import { log4 } from './log4';
+import { opcodes as logOpcodes } from './log';
 
 export const opcodes: Record<number, Partial<Opcode>> = {
-  [log0.number]: log0,
-  [log1.number]: log1,
-  [log2.number]: log2,
-  [log3.number]: log3,
-  [log4.number]: log4,
+  ...logOpcodes,
 };
