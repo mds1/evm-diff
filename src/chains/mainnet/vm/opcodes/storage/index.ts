185972a2ddf6094bbd89a91f648a988753fe5194
leovct <l009.vincent@gmail.com>
2023-06-19 00:36:28 +0200

    feat: add `storage` opcodes

--- src/chains/mainnet/vm/opcodes/storage/index.ts
+++ src/chains/mainnet/vm/opcodes/storage/index.ts
@@ -1,3 +1,8 @@
 import { Opcode } from '@/types';
+import { sload } from './sload';
+import { sstore } from './sstore';
 
-export const opcodes: Record<number, Opcode> = {};
+export const opcodes: Record<number, Opcode> = {
+  [sload.number]: sload,
+  [sstore.number]: sstore,
+};
