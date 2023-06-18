0e74be6a1a90253cd0bd820575c05d5cc8b20267
leovct <l009.vincent@gmail.com>
2023-06-19 01:23:34 +0200

    feat: add 1/3 of `environment` opcodes

--- src/chains/mainnet/vm/opcodes/environment/index.ts
+++ src/chains/mainnet/vm/opcodes/environment/index.ts
@@ -1,3 +1,12 @@
 import { Opcode } from '@/types';
+import { address } from './address';
+import { balance } from './balance';
+import { caller } from './caller';
+import { origin } from './origin';
 
-export const opcodes: Record<number, Opcode> = {};
+export const opcodes: Record<number, Opcode> = {
+  [address.number]: address,
+  [balance.number]: balance,
+  [caller.number]: caller,
+  [origin.number]: origin,
+};
