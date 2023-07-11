b7bb1b5e0bd253fa5ddf5b601500c0dd05c1f6d1
leovct <l009.vincent@gmail.com>
2023-07-11 10:52:24 +0200

    chore: lint

--- src/chains/arbitrum/vm/opcodes/environment/caller.ts
+++ src/chains/arbitrum/vm/opcodes/environment/caller.ts
@@ -4,7 +4,7 @@ import { Opcode } from '@/types';
 export const caller: Omit<Opcode, 'minGas' | 'examples' | 'errorCases' | 'supportedHardforks'> = {
   ...callerMainnet,
   description:
-  'If the transaction is an L1 ⇒ L2 transaction, and this is the initial call (rather than an internal transaction from one contract to another), then `msg.sender` is set to the aliased address of the address that triggered the L1 ⇒ L2 transaction. Otherwise, this opcode behaves normally.',
+    'If the transaction is an L1 ⇒ L2 transaction, and this is the initial call (rather than an internal transaction from one contract to another), then `msg.sender` is set to the aliased address of the address that triggered the L1 ⇒ L2 transaction. Otherwise, this opcode behaves normally.',
   references: [
     ...callerMainnet.references,
     {
