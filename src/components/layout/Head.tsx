520544f496c961e8c2edad4ff7b539ecf708ff1c
leovct <l009.vincent@gmail.com>
2023-08-04 18:39:49 +0200

    chore: enable social preview on twitter

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -1,5 +1,12 @@
 import NextHead from 'next/head';
-import { OG_ENDPOINT, SITE_DESCRIPTION, SITE_NAME, SITE_URL } from '@/lib/constants';
+import {
+  COMPANY_URL,
+  OG_ENDPOINT,
+  SITE_DESCRIPTION,
+  SITE_NAME,
+  SITE_URL,
+  TWITTER_URL,
+} from '@/lib/constants';
 
 interface Props {
   title?: string;
@@ -30,10 +37,17 @@ export const Head = (props: Props) => {
       <title>{props.title ? `${title} | ${SITE_NAME}` : SITE_NAME}</title>
       <meta name='description' content={description ?? SITE_DESCRIPTION} />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+
+      <meta property='og:title' content={SITE_NAME} />
+      <meta property='og:description' content={SITE_DESCRIPTION} />
       <meta
         property='og:image'
         content={`${SITE_URL}${OG_ENDPOINT}${getQueryParams(base, target)}`}
       />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site' content={COMPANY_URL} />
+      <meta name='twitter:creator' content={TWITTER_URL} />
     </NextHead>
   );
 };
