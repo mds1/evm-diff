52d6a2eab03059c7555b32a516cf6824fdc23a9a
Matt Solomon <matt@mattsolomon.dev>
2023-08-08 16:06:56 -0700

    fix: locked ReadableStream error
    removes the following error on og image request:
    This stream has already been locked for exclusive reading by another reader

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -3,8 +3,8 @@ import { ImageResponse } from '@vercel/og';
 import { getAddress } from 'viem';
 import { getChainById } from '@/chains';
 import { convertToComparableOpcode } from '@/components/diff/DiffOpcodes';
-import { COMPANY_NAME, SITE_DESCRIPTION, SITE_NAME } from '@/lib/constants';
-import { Opcode, Precompile, Predeploy, SignatureType } from '@/types';
+import { SITE_DESCRIPTION } from '@/lib/constants';
+import { Chain, Opcode, Precompile, Predeploy, SignatureType } from '@/types';
 
 export const config = {
   runtime: 'edge',
@@ -70,69 +70,56 @@ const countSignatureTypeDiffs = (base: SignatureType[], target: SignatureType[])
   return countDifferences(base, target, getKey, isEqual);
 };
 
-const defaultImageResponse = new ImageResponse(
-  (
-    <div
-      style={{
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white',
-      }}
-    >
-      <h2
-        style={{
-          fontSize: 100,
-        }}
-      >
-        {`✨ ${SITE_NAME} ✨`}
-      </h2>
-      <p
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontSize: 34,
-        }}
-      >
-        <p>{SITE_DESCRIPTION}</p>
-        <p>Made by {COMPANY_NAME}</p>
-      </p>
-    </div>
-  ),
-  {
-    width: 1200,
-    height: 630,
-  }
-);
-
 export default async function handler(request: NextRequest) {
-  try {
-    const imageData = await fetch(new URL('public/logo-dark-tight.png', import.meta.url)).then(
-      (res) => res.arrayBuffer()
+  const getLogoImageData = async () => {
+    return await fetch(new URL('public/logo-dark-tight.png', import.meta.url)).then((res) =>
+      res.arrayBuffer()
     );
+  };
+  const generateDefaultImage = async () => {
+    const imageData = await getLogoImageData();
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#f4f4f5',
+          }}
+        >
+          <h2
+            style={{
+              fontSize: 100,
+            }}
+          >
+            <img width='400' src={imageData} />
+          </h2>
+          <p
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              fontSize: 34,
+            }}
+          >
+            <p>{SITE_DESCRIPTION}</p>
+          </p>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  };
 
-    // We need to access via `nextUrl` instead of just `url` to fix the following error:
-    //   Error [TypeError]: This stream has already been locked for exclusive reading by another reader
-    // More info: https://github.com/vercel/next.js/issues/48403
-    const searchParams = request.nextUrl.searchParams;
-    // const req = await request.json();
-    // const { searchParams } = new URL(req.url);
-
-    const base = searchParams.get('base'); // ?base=<base>
-    const target = searchParams.get('target')?.slice(0, 100); // ?target=<target>
-
-    if (!base || !target) return defaultImageResponse;
-
-    const baseChain = getChainById(base as string);
-    if (baseChain === undefined) return defaultImageResponse;
-
-    const targetChain = getChainById(target as string);
-    if (targetChain === undefined) return defaultImageResponse;
+  const generateDiffImage = async (baseChain: Chain, targetChain: Chain) => {
+    const imageData = await getLogoImageData();
 
     const precompileDiffs = countPrecompilesDiff(baseChain.precompiles, targetChain.precompiles);
     const predeployDiffs = countPredeployDiffs(baseChain.predeploys, targetChain.predeploys);
@@ -156,7 +143,7 @@ export default async function handler(request: NextRequest) {
             flexDirection: 'column',
             alignItems: 'center',
             justifyContent: 'center',
-            backgroundColor: 'white',
+            backgroundColor: '#f4f4f5',
             paddingTop: '50px',
           }}
         >
@@ -197,11 +184,25 @@ export default async function handler(request: NextRequest) {
         height: 630,
       }
     );
+  };
+
+  try {
+    const { searchParams } = request.nextUrl;
+    const base = searchParams.get('base'); // ?base=<base>
+    const target = searchParams.get('target')?.slice(0, 100); // ?target=<target>
+    if (!base || !target) return generateDefaultImage();
+
+    const baseChain = getChainById(base as string);
+    if (baseChain === undefined) return generateDefaultImage();
+
+    const targetChain = getChainById(target as string);
+    if (targetChain === undefined) return generateDefaultImage();
+
+    return await generateDiffImage(baseChain, targetChain);
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
+    console.log(`OG image generation failed with error: ${e.message}`);
+    return generateDefaultImage();
   }
 }
