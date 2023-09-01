6e11248edb0543fe84aed04f2793cbe9f90f6b38
Matt Solomon <matt@mattsolomon.dev>
2023-08-31 19:17:50 -0700

    chore: add more mempools

--- src/types/mempool.ts
+++ src/types/mempool.ts
@@ -4,16 +4,17 @@ export type Mempool = {
   rpcUrl: string;
   references: string[];
   notes?: string[];
+  // All properties are private, and a missing field indicates that the property is unknown.
   properties: {
-    isPrivate: boolean;
-    tracksIpAddress: boolean;
-    refundsMev: boolean;
-    includesFailedTxs: boolean;
-    canSpecifyBuilders: boolean;
-    isFree: boolean;
-    configurable: boolean;
-    txLifespan: number; // Seconds until kicked from mempool.
-    rateLimit: string;
-    burstRateLimit: string;
+    isPrivate?: boolean;
+    tracksIpAddress?: boolean;
+    refundsMev?: boolean;
+    includesFailedTxs?: boolean;
+    canSpecifyBuilders?: boolean;
+    isFree?: boolean;
+    configurable?: boolean;
+    txLifespan?: number; // Seconds until kicked from mempool.
+    rateLimit?: string;
+    burstRateLimit?: string;
   };
 };
