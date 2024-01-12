5e1b7e2151cd9e27e3a14cd0280a7e6fc0e305b6
leovct <l009.vincent@gmail.com>
2024-01-12 17:35:59 +0100

    chore: small changes to `format`

--- src/components/diff/utils/format.tsx
+++ src/components/diff/utils/format.tsx
@@ -2,31 +2,30 @@ import { MainnetHardfork } from '@/chains/mainnet/hardforks';
 import { CURRENT_MAINNET_HARDFORK } from '@/lib/constants';
 
 export const formatHardfork = (array: string[]): JSX.Element => {
-  if (array == undefined || array.length == 0) {
+  if (!array || array.length == 0) {
     return <p>No information provided on supported hard forks.</p>;
   }
 
+  const [first, ...rest] = array;
+  const last = rest.pop();
   const length = array.length;
   const currentMainnetHardforkName = MainnetHardfork[CURRENT_MAINNET_HARDFORK];
   if (length == 1) {
     return (
       <p>
-        Supported only in <b>{array[0]}</b> hard fork.
+        Supported only in <b>{first}</b> hard fork.
       </p>
     );
-  } else if (
-    length == CURRENT_MAINNET_HARDFORK + 1 ||
-    array[length - 1] === currentMainnetHardforkName
-  ) {
+  } else if (length == CURRENT_MAINNET_HARDFORK + 1 || last === currentMainnetHardforkName) {
     return (
       <p>
-        Supported since <b>{array[0]}</b> hard fork.
+        Supported since <b>{first}</b> hard fork.
       </p>
     );
   }
   return (
     <p>
-      Supported between <b>{array[0]}</b> and <b>{array[length - 1]}</b> hard forks.
+      Supported between <b>{first}</b> and <b>{last}</b> hard forks.
     </p>
   );
 };
