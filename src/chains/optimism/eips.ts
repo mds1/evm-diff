06536183623541192ea7e64047745f1746db0365
leovct <l009.vincent@gmail.com>
2024-01-24 16:42:46 +0100

    chore: remove eip6049 from optimsim eips

--- src/chains/optimism/eips.ts
+++ src/chains/optimism/eips.ts
@@ -1,6 +1,5 @@
-import { EIP, EIPState } from '@/types/eip';
+import { EIP } from '@/types/eip';
 import { eip1559 as eip1559OnEthereum, eips as ethereumEIPs } from '../mainnet/eips';
-import { MainnetHardfork, getHardforksFrom } from '../mainnet/hardforks';
 
 const eip1559OnOptimism: EIP = {
   ...eip1559OnEthereum,
@@ -24,25 +23,9 @@ const eip1559OnOptimism: EIP = {
   ],
 };
 
-const hardforksFromShanghai: string[] = getHardforksFrom(MainnetHardfork.Shanghai);
-const eip6049: EIP = {
-  number: 6049,
-  title: 'Deprecate SELFDESTRUCT',
-  status: EIPState.Final,
-  activeHardforks: hardforksFromShanghai,
-  references: [
-    'https://eips.ethereum.org/EIPS/eip-6049',
-    'https://github.com/ethereum-optimism/optimism/blob/develop/specs/superchain-upgrades.md#canyon',
-  ],
-};
-
 export const eips = ethereumEIPs
   .map((eip) => {
     // EIPs present on Ethereum but modified on Optimism.
     if (eip.number === 1559) return eip1559OnOptimism;
     return eip;
-  })
-  .concat(
-    // EIPs present on Optimism but not on Ethereum.
-    eip6049
-  );
+  });
