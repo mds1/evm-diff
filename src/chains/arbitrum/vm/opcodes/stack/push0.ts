47560c53031d8709c66006681e1d825f25ecd3a5
Matt Solomon <matt@mattsolomon.dev>
2024-02-02 06:10:58 -0800

    feat: arbitrum now supports push0 (#58)

--- src/chains/arbitrum/vm/opcodes/stack/push0.ts
+++ src/chains/arbitrum/vm/opcodes/stack/push0.ts
@@ -1,12 +1,7 @@
 import { push0 as baseOpcode } from '@/chains/mainnet/vm/opcodes/stack/push';
 import { Opcode } from '@/types';
 
-const { supportedHardforks: _supportedHardforks, ...opcode } = baseOpcode;
-export const push0: Omit<Opcode, 'supportedHardforks'> = {
-  ...opcode,
-  description:
-    'The opcode is not supported yet, but will be added in a future hardfork. This means you cannot yet use Solidity 0.8.20 or later with an `evm_version` of Shanghai.',
-  references: [
-    '[Arbitrum Differences from Solidity on Ethereum](https://developer.arbitrum.io/solidity-support)',
-  ],
+export const push0: Opcode = {
+  ...baseOpcode,
+  supportedHardforks: ['ArbOS 11'],
 };
