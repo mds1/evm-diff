0bb8af60a0b55d5918d0e069a0acb147fd8e84b4
leovct <l009.vincent@gmail.com>
2024-02-05 14:14:15 +0100

    chore: update arbitrum eip diffs

--- src/chains/arbitrum/eips.ts
+++ src/chains/arbitrum/eips.ts
@@ -2,6 +2,7 @@ import { EIP, EIPCategory } from '@/types/eip';
 import {
   eip4399 as eip1399OnMainnet,
   eip1559 as eip1559OnMainnet,
+  eip4895 as eip4895OnMainnet,
   eips as ethereumEIPs,
 } from '../mainnet/eips';
 import { ArbitrumHardfork, getArbitrumHardforksFrom } from './hardforks';
@@ -13,6 +14,9 @@ const eip1559OnArbitrum: EIP = {
   parameters: [],
   notes: [
     'EIP-1599 is not implemented on Arbitrum but Arbitrum implements an "exponential mechanism" which has been shown equivalent to Ethereum\'s EIP-1559 gas pricing mechanism.',
+    'EIP-1559 is partially implemented with the following exceptions:',
+    '- Target a gas per second ("speed limit") instead of block gas limit to account for the variable block time in Arbitrum.',
+    '- Priority fee is not collected by the network even when nonzero value is specified.',
   ],
   references: [
     ...eip1559OnMainnet.references,
@@ -26,6 +30,15 @@ const eip4399OnArbitrum: EIP = {
   references: [...eip1399OnMainnet.references, 'https://developer.arbitrum.io/solidity-support'],
 };
 
+const eip4895OnArbitrum: EIP = {
+  ...eip4895OnMainnet,
+  notes: ['Arbitrum currently do not generate block with withdrawalsRoot.'],
+  references: [
+    ...eip4895OnMainnet.references,
+    'https://github.com/mds1/evm-diff/pull/60#issuecomment-1926794404',
+  ],
+};
+
 export const eips: EIP[] = ethereumEIPs
   .filter((eip) => {
     // Exclude consensus-related EIPs.
@@ -35,5 +48,6 @@ export const eips: EIP[] = ethereumEIPs
     // EIPs modified by Arbitrum hard forks.
     if (eip.number === 1559) return eip1559OnArbitrum;
     if (eip.number === 4399) return eip4399OnArbitrum;
+    if (eip.number === 4895) return eip4895OnArbitrum;
     return eip;
   });
