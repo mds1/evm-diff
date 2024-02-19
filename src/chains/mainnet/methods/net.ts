f2aa4a9c89497522e89ad45065d887621ad85a67
leovct <l009.vincent@gmail.com>
2024-02-19 11:14:43 +0100

    feat: add net methods

--- src/chains/mainnet/methods/net.ts
+++ src/chains/mainnet/methods/net.ts
@@ -1,4 +1,55 @@
-import { Method } from "@/types";
+import { Method, MethodNamespace } from "@/types";
 
-// TODO: net namespace.
-export const netMethods: Method[] = [];
+const netVersion: Method = {
+  name: 'net_version',
+  namespace: MethodNamespace.Net,
+  description: 'Returns the current network id',
+  return: {
+    type: 'string',
+    description: 'The current network id',
+  },
+  example: {
+    parameters: [],
+    result: '3',
+  },
+  references: [
+    '[ethereum.org](https://ethereum.org/en/developers/docs/apis/json-rpc#net_version)',
+    '[Full list of curent network IDs](https://chainlist.org/)',
+  ]
+}
+
+const netListening: Method = {
+  name: 'net_listening',
+  namespace: MethodNamespace.Net,
+  description: 'Returns true if client is actively listening for network connections',
+  return: {
+    type: 'boolean',
+    description: 'Return true when listening, otherwise false',
+  },
+  example: {
+    parameters: [],
+    result: 'true',
+  },
+  references: [
+    '[ethereum.org](https://ethereum.org/en/developers/docs/apis/json-rpc#net_listening)',
+  ]
+}
+
+const netPeerCount: Method = {
+  name: 'net_peercount',
+  namespace: MethodNamespace.Net,
+  description: 'Returns number of peers currently connected to the client',
+  return: {
+    type: 'quantity',
+    description: 'Integer of the number of connected peers',
+  },
+  example: {
+    parameters: [],
+    result: '0x2',
+  },
+  references: [
+    '[ethereum.org](https://ethereum.org/en/developers/docs/apis/json-rpc#net_peercount)',
+  ]
+}
+
+export const netMethods: Method[] = [netVersion, netListening, netPeerCount];
