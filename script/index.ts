4eab5203b288883ad58417a4da3bb376f27469a8
Matt Solomon <matt@mattsolomon.dev>
2024-04-13 11:14:45 -0700

    organize script folder

---
+++ script/index.ts
@@ -0,0 +1,4 @@
+/**
+ * Takes a chain ID as an input, fetches data from https://chainid.network/chains.json, uses that
+ * to read and infer various data and properties of the chain, and writes the result to `data/${chainId}.toml`.
+ */
