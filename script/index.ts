b750baf1be9331589330c557e4353b6792458c9c
Matt Solomon <matt@mattsolomon.dev>
2024-04-14 18:15:31 -0700

    feat: check opcodes

--- script/index.ts
+++ script/index.ts
@@ -1,12 +1,29 @@
+import { http, createPublicClient } from 'viem';
+import { checkOpcodes } from './opcodes';
 import type { Metadata } from './types';
+
+async function main() {
+	const { chainId } = init();
+	const metadata = await getMetadata(chainId);
+	const rpcUrl = selectRpcUrl(metadata.rpc);
+	const client = initClient(rpcUrl);
+	const opcodes = await checkOpcodes(client);
+	const chain = { metadata, opcodes };
+	await save(chainId, chain);
+}
 
 function init() {
 	const arg = process.argv[2];
 	if (!arg) throw new Error('Must provide chainId as an argument');
 	const chainId = Number(arg);
 	if (Number.isNaN(chainId)) throw new Error('chainId must be a number');
-	if (chainId > 0 && chainId < Number.MAX_SAFE_INTEGER) throw new Error('chainId not supported');
+	if (chainId <= 0 || chainId > Number.MAX_SAFE_INTEGER) throw new Error('chainId not supported');
 	return { chainId };
+}
+
+function initClient(rpcUrl: string) {
+	const transport = http(rpcUrl);
+	return createPublicClient({ transport });
 }
 
 async function getMetadata(chainId: number): Promise<Metadata> {
@@ -22,11 +39,32 @@ async function save(chainId: number, chainObj: object) {
 	console.log(`✅ Chain data for chainId ${chainId} written to data/${chainId}.json`);
 }
 
-async function main() {
-	const { chainId } = init();
-	const metadata = await getMetadata(chainId);
-	const chain = { metadata };
-	await save(chainId, chain);
+function selectRpcUrl(rpcUrls: string[]): string {
+	const hasPlaceholder = (str: string): boolean => str.includes('${');
+
+	const replacePlaceholder = (str: string): string => {
+		const placeholderRegex = /\$\{(\w+)\}/;
+		const match = str.match(placeholderRegex);
+		if (match) {
+			const apiKey = process.env[match[1]];
+			if (apiKey) return str.replace(placeholderRegex, apiKey);
+		}
+		return str;
+	};
+
+	// Check for URLs with placeholders and replace with API key if available. In this case, we
+	// return this URL as the selected RPC URL. Preferring URLs with API keys this way helps
+	// avoid rate limits and other issues.
+	const httpUrls = rpcUrls.filter((url) => !url.startsWith('wss://'));
+	for (const url of httpUrls) {
+		if (hasPlaceholder(url)) {
+			const replacedUrl = replacePlaceholder(url);
+			if (replacedUrl !== url) return replacedUrl;
+		}
+	}
+
+	// Otherwise, return the first URL that does not have a placeholder.
+	return httpUrls.find((url) => !hasPlaceholder(url)) || '';
 }
 
 main().catch((error) => {
